0a3b2ef60fdabfc6a7de6b651505e9a1d86de5bd
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-07-22 16:33:31 +0530

    Add files via upload

---
+++ operators/arithmetic opeators/single_level1.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class single_level1 {
+
+	public void print()
+	{
+			System.out.println("Single level Inheritance");
+}
+}
+
+
---
+++ operators/arithmetic opeators/single_level2.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class single_level2 extends single_level1 {
+
+	public static void main(String [] args) {
+		
+		single_level1 obj = new single_level1();
+		
+		obj.print();
+	} 
+}
