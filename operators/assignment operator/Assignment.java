201290fae87b6cd501579df180d4c259004d264b
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-07-27 16:05:42 +0530

    Add files via upload

---
+++ operators/assignment operator/Assignment.java
@@ -0,0 +1,29 @@
+package Operator;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Assignment Operator");
+        int a=10;
+        int b=2;
+        int c = 8;
+        //a+=b;
+        //System.out.println(a);
+        
+//        a-=b;
+//        a+=b;
+
+//        a/=b;
+        
+       
+        a*=b;     
+  System.out.println(a);      
+	              c/=a; 
+	              System.out.println(c);
+
+        
+        
+	}
+
+}
