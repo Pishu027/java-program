0e4e33b4f11af473aca91b82384859aa7163e1fa
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-07-27 16:07:51 +0530

    Add files via upload

---
+++ operators/arithmetic operator/Arithmetic.java
@@ -0,0 +1,30 @@
+package Operator;
+
+public class Arithmetic {
+
+
+	public static void main(String[] args)
+	{
+	System.out.println("Arithmetic Operators");
+	
+	int a,b,c;
+	a=43;
+	b=5;
+	c=a+b;
+	System.out.println(c);
+	
+	c=a-b;
+	System.out.println(c);
+	
+	c=a*b;
+	System.out.println(c);
+	
+	c=a%b;
+	System.out.println(c);
+	
+	c=a/b;
+	System.out.println(c);
+	
+	}
+}
+
