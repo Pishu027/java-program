d38700bf60db85dc2f945823464feaada27b80c1
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-07-30 08:32:38 +0530

    Add files via upload

---
+++ operators/ternary/ternary.java
@@ -0,0 +1,18 @@
+package Operator;
+
+public class ternary {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ternary Operator");
+		
+		int a=20, b=15;
+		System.out.println("Value of A =  "+a);
+
+		System.out.println("Value of B =  "+b);
+		
+	int r=	(a<b?b:a);
+	System.out.println(r);
+	}
+
+}
