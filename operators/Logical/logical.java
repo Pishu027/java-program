adcd387c9b42520131e18bce3215a13db33642a3
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-07-30 08:31:01 +0530

    Add files via upload

---
+++ operators/Logical/logical.java
@@ -0,0 +1,22 @@
+package Operator;
+
+public class logical {
+
+	public static void main(String[] args){
+	 	
+	         int age = 2;
+	         
+	         if(age>18 || age<25) 
+	         {
+	              System.out.println("You Can Vote");	 
+	         }
+	         else {
+	        	 System.out.println("You Can't Vote");
+	         }
+	}
+	
+	
+	
+	
+	
+}
