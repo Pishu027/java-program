ab2ad827a97643d58d20dc4116680f2ed884f343
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-08-06 15:51:54 +0530

    Create Calling_threads.java

---
+++ Assignment/Thur(04-08-22)/INFINITY_THREAD/Calling_threads.java
@@ -0,0 +1,18 @@
+// Step 3:
+
+package lab04_08;
+
+public class Calling_threads {
+
+	public static void main(String[] args)
+	     {    //creating objects
+	          Thread1 t1 = new Thread1("Running Thread1....");
+	          Thread1 t2 = new Thread1("Running Thread2....");
+	          
+	          //calling run method using start method
+	          t1.start();
+	          t2.start();
+	     }
+
+	}
+
