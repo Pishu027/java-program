1517c3a0ae07e5c379c1d22f381188a643d41ad2
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-09-20 19:25:32 +0530

    Create Hierarchical.java

---
+++ Assignment/14-07-2022/Hierarchical/Hierarchical.java
@@ -0,0 +1,24 @@
+package demo;
+import java.util.Scanner;
+
+public class Hierarchical
+{
+	Scanner sc=new Scanner(System.in);
+	int x;
+	int y;
+
+	public void accept() 
+	{
+		//taking input from user
+		
+		System.out.println("Enter the first number");//first input
+		x=sc.nextInt();
+		System.out.println("The value of x is:");
+		
+		System.out.println("Enter the second number");//second input
+		y=sc.nextInt();
+		System.out.println("The value of y is:");
+		
+	}
+
+}
