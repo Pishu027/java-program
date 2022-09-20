12780ca62803031922305ae325b9fa1c4eeae4b5
Prashant <107205829+Pishu027@users.noreply.github.com>
2022-09-20 19:21:49 +0530

    Create Encapsulation.java

---
+++ Assignment/14-07-2022/Encapsulation/Encapsulation.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class Encapsulation {
+	private double percentage;
+	private char grade;
+
+	public void setPercentage(double percentage)
+	{
+		this.percentage = percentage;
+	}
+	
+	public double getPercentage() 
+	{
+		return percentage;
+	}
+
+	public char getGrade() {
+		return grade;
+	}
+
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}
+	
+}
